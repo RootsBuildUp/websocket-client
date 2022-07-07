20af5a6ef76ff7029bd818241c11879db855ea2c
rashedfci994 <rashed@businessaccelerate.com.bd>
2022-07-07 18:13:49 +0600

    first commit

---
+++ src/main/java/client/StompClient.java
@@ -0,0 +1,30 @@
+package client;
+
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.simp.stomp.StompSessionHandler;
+import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
+import org.springframework.web.socket.client.WebSocketClient;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+import org.springframework.web.socket.messaging.WebSocketStompClient;
+
+import java.util.Scanner;
+
+/**
+ * Stand alone WebSocketStompClient.
+ *
+ */
+public class StompClient {
+    
+    public static void main(String[] args) {
+        WebSocketClient webSocketClient = new StandardWebSocketClient();
+        WebSocketStompClient stompClient = new WebSocketStompClient(webSocketClient);
+        stompClient.setMessageConverter(new MappingJackson2MessageConverter());
+        stompClient.setTaskScheduler(new ConcurrentTaskScheduler());
+
+        String url = "ws://127.0.0.1:8080/hello";
+        StompSessionHandler sessionHandler = new MySessionHandler();
+        stompClient.connect(url, sessionHandler);
+
+        new Scanner(System.in).nextLine(); //Don't close immediately.
+    }
+}
